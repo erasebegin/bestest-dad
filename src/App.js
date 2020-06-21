6c74a3951085362ef2f7135fd1402c5676f67811
erasebegin <chris.james.haupt@gmail.com>
2020-06-21 08:47:39 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled from 'styled-components';
+import ReactAudioPlayer from 'react-audio-player';
+import dadSong from './daddy-song-v2.mp3';
+import dadImage from './dad-simon-and-me.png'
+import Gallery from './Gallery';
+
+const App = () => {
+  return (
+    <>
+    <Main>
+      <h1>
+        Because you're the best dad in the world...
+      </h1>
+      <img className="drawing" src={dadImage}></img>
+      <h1>
+        We wrote a song for you.
+      </h1>
+      <h1>
+        It's called Daddy's Song.
+      </h1>
+      <ReactAudioPlayer 
+      src={dadSong}
+      controls
+      /> 
+      <h2>While you listen, here are some pictures</h2>   
+      <h2>To remind you of the time when we no words are has</h2>
+      <Gallery />
+    </Main>
+    </>
+  );
+};
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: 'Indie Flower', cursive;
+  background-image: url(https://i.ibb.co/z2252ts/paper-background-o.jpg);
+  background-repeat: repeat;
+  padding: 1em;
+  text-align: center;
+
+  h1 {
+    color: rgb(26, 14, 0);
+    font-size: 4rem;
+  }
+
+  h2 {
+    font-size: 3rem;
+  }
+
+  .drawing {
+    border-radius: 10px;
+    max-width: 90vw;
+    max-height: 90vh;
+  }
+`
+
+export default App;
+
+ {/* #1200A3 Neutral
+ #2D2375 Unsaturated
+ #0B005C Deepest
+ #4032A8 Light
+ #1D00F5 Brightest */}
---
+++ src/Gallery.js
@@ -0,0 +1,67 @@
+import React, {useState} from "react";
+import styled from "styled-components";
+
+export default function Gallery() {
+
+  const imgArr = [
+    "https://i.ibb.co/W02vN00/104466996-714155316017680-3200005302407271214-n.jpg",
+    "https://i.ibb.co/z6cq8nw/104431816-642579943012986-2288021561591194988-n.jpg",
+    "https://i.ibb.co/XV334GY/104287052-630025824389903-3659960106707765370-n.jpg",
+    "https://i.ibb.co/VN8NgPZ/104338424-1151124005242806-2803023027736328480-n.jpg",
+    "https://i.ibb.co/6nHcMRz/104469816-261739398471494-3179166702810626944-n.jpg",
+    "https://i.ibb.co/Dfmx6Kk/104430904-847855915740875-1592402461122480070-n.jpg",
+    "https://i.ibb.co/MsSSTzr/104333719-1183551925323858-366756681526771127-n.jpg",
+    "https://i.ibb.co/3FtWvV7/104136121-2716603811891458-947477019921470155-n.jpg",
+    "https://i.ibb.co/LRvD9GN/104339516-735304453909424-5827990947201342497-n.jpg"
+  ];
+  const [currImg, setCurrImg] = useState(0)
+
+  return (
+    <GalleryContainer>
+      <div className="subcontainer">
+        <img className="gallery-image" src={imgArr[0]} onClick={()=> setCurrImg(0)}></img>
+        <img className="gallery-image" src={imgArr[1]} onClick={()=> setCurrImg(1)}></img>
+        <img className="gallery-image" src={imgArr[2]} onClick={()=> setCurrImg(2)}></img>
+        <img className="gallery-image" src={imgArr[3]} onClick={()=> setCurrImg(3)}></img>
+        <img className="gallery-image" src={imgArr[4]} onClick={()=> setCurrImg(4)}></img>
+        <img className="gallery-image" src={imgArr[5]} onClick={()=> setCurrImg(5)}></img>
+        <img className="gallery-image" src={imgArr[6]} onClick={()=> setCurrImg(6)}></img>
+        <img className="gallery-image" src={imgArr[7]} onClick={()=> setCurrImg(7)}></img>
+        <img className="gallery-image" src={imgArr[8]} onClick={()=> setCurrImg(8)}></img>
+      </div>
+      <img className = "main-image" src={imgArr[currImg]}></img>
+    </GalleryContainer>
+  );
+}
+
+const GalleryContainer = styled.div`
+  margin: 3em;
+
+  .main-image {
+    max-width: 90vw;
+    max-height: 90vh;
+    object-fit: cover;
+  }
+
+  .gallery-image {
+    max-width: 100px;
+    object-fit: cover;
+    transition: 200ms;
+    margin: 0 .5em;
+    border-radius: 8px;
+
+    &:hover {
+      transform: translate(-2px, -1px);
+      transition: 200ms;
+      cursor: pointer;
+      filter: contrast(1.5);
+    }
+  }
+
+  .subcontainer {
+    background: black;
+    padding: .5em;
+    margin: .5em;
+    border-radius: 10px;
+  }
+`;
